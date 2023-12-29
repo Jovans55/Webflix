64f1ffa4228be673478f8485ae5ed01f05a0c9df
Jovan <jovanstosic012@gmail.com>
2023-12-29 15:48:04 -0600

    first commit!

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Welcome to</h1>
+      <img src='./public/webflix.png' alt='Webflix logo ligt blue' />
+    </div>
+  )
+}
+
+export default App
