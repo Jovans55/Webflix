ca28b36efc72bbace97bfd13862d0288b9cec8fd
Jovan <jovanstosic012@gmail.com>
2023-12-29 22:16:35 -0600

    Changes,  made everyhting look way better fixed the sign in css:

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ function App() {
   return (
     <div className="appContainer">
       <header>
-        <img src='./public/webflix.png' alt='Webflix logo light blue' width={"200px"} height={"100%"} />
+        <img src='/webflix.png' alt='Webflix logo light blue' width={"200px"} height={"100%"} style={{margin: "0px 15px"}}/>
       </header>
       <section id='signInContainer'>
         <div id='signIn'>
-          <h1>Sign In</h1>
-          <input style={{display:"block"}} placeholder='Email'></input>
-          <input style={{display:"block"}} placeholder='Password'></input>
-          <button>Sign In</button>
+          <h1 style={{fontSize: "30px"}}>Sign In</h1>
+          <input className="inputField" style={{display:"block"}} placeholder='Email'></input>
+          <input className="inputField" style={{display:"block"}} placeholder='Password' type="password"></input>
+          <button style={{color: "white", backgroundColor: "#399afb", width: "300px", padding: "10px 15px", border: "none",borderRadius: "5px", margin: "30px 0px 5px", fontSize: "17px"}}>Sign In</button>
           <div>
             <input type="checkbox" name="Remeber me" value="Remeber" />
-            <label>Remember me</label>
+            <label style={{marginRight: "80px"}}>Remember me</label>
+            <label>Need help?</label>
           </div>
         </div>
       </section>
