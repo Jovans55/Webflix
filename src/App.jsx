bc6c081d7576f3b7341ac2992d1d6564dce212e8
Jovan <jovanstosic012@gmail.com>
2023-12-29 19:22:02 -0600

    Changes created basic html layout and made some css changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,15 @@ function App() {
       <header>
         <img src='./public/webflix.png' alt='Webflix logo ligt blue' width={"200px"} height={"100%"}/>
       </header>
+      <section>
+        <h1>Sign In</h1>
+        <input placeholder='Email'></input>
+        <input placeholder='Password'></input>
+        <button>Sign In</button>
+      </section>
+      <footer>
+        <h1>FOOTER</h1>
+      </footer>
     </div>
   )
 }
