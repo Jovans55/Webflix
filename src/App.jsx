4274d112a1ed62476d9180318f1172840a0c02e2
Jovan <jovanstosic012@gmail.com>
2023-12-29 23:56:54 -0600

    CHANGES made the footer larger finished the sign in now need to add function

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,27 @@ function App() {
   return (
     <div className="appContainer">
       <header>
-        <img src='/webflix.png' alt='Webflix logo light blue' width={"200px"} height={"100%"} style={{margin: "0px 15px"}}/>
+        <img src='/webflix.png' alt='Webflix logo light blue' width={"200px"} height={"100%"} style={{margin: "10px 0px 0px 15px"}}/>
       </header>
       <section id='signInContainer'>
         <div id='signIn'>
-          <h1 style={{fontSize: "30px"}}>Sign In</h1>
-          <input className="inputField" style={{display:"block"}} placeholder='Email'></input>
-          <input className="inputField" style={{display:"block"}} placeholder='Password' type="password"></input>
-          <button style={{color: "white", backgroundColor: "#399afb", width: "300px", padding: "10px 15px", border: "none",borderRadius: "5px", margin: "30px 0px 5px", fontSize: "17px"}}>Sign In</button>
-          <div>
+          <h1 style={{fontSize: "32px"}}>Sign In</h1>
+          <input className="inputField" placeholder='Email'></input>
+          <input className="inputField" placeholder='Password' type="password"></input>
+          <button style={{color: "white", backgroundColor: "#399afb", width: "300px", padding: "10px 15px", border: "none", borderRadius: "5px", margin: "30px 0px 10px", fontSize: "17px"}}>Sign In</button>
+          <div style={{marginBottom: "50px"}}>
             <input type="checkbox" name="Remeber me" value="Remeber" />
             <label style={{marginRight: "80px"}}>Remember me</label>
-            <label>Need help?</label>
+            <a>Need help?</a>
           </div>
+          <p>New to Webflix?</p>
+          <a>Sign Up!</a>
         </div>
       </section>
       <footer>
-        <h1>FOOTER</h1>
+        <div>
+          <h1>FOOTER</h1>
+        </div>
       </footer>
     </div>
   );
