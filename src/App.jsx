15bfabbf969bafca1a5b335836e9ac72e00a023b
Jovan <jovanstosic012@gmail.com>
2023-12-30 00:37:48 -0600

    CHANGES made the footer finallly looks good and change the padding on some stuff to make things look a little better

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
           <input className="inputField" placeholder='Password' type="password"></input>
           <button style={{color: "white", backgroundColor: "#399afb", width: "300px", padding: "10px 15px", border: "none", borderRadius: "5px", margin: "30px 0px 10px", fontSize: "17px"}}>Sign In</button>
           <div style={{marginBottom: "50px"}}>
-            <input type="checkbox" name="Remeber me" value="Remeber" />
+            <input type="checkbox" name="Remember me" value="Remember" />
             <label style={{marginRight: "80px"}}>Remember me</label>
             <a>Need help?</a>
           </div>
@@ -22,7 +22,24 @@ function App() {
       </section>
       <footer>
         <div>
-          <h1>FOOTER</h1>
+          <p>Questions? Call 1-123-456-789</p>
+          <section>
+            <div>
+              <a>FAQ</a>
+              <a>Help Center</a>
+              <a>Webflix Shop</a>
+            </div>
+            <div>
+              <a>Terms of Use</a>
+              <a>Privacy</a>
+              <a>Cookie Preferences</a>
+            </div>
+            <div>
+              <a>Corporate Information</a>
+              <a>Do Not Sell or Share My Personal Informatoin</a>
+              <a>Ad Choices</a>
+            </div>
+          </section>
         </div>
       </footer>
     </div>
