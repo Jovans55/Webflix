c7278c151427ee832599397ec0b08846507e7973
Jovan <jovanstosic012@gmail.com>
2023-12-30 18:06:16 -0600

    Testing out mongo db and setting up scheme and everything to work for users

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,10 +1,41 @@
 import express from "express";
 const router = express.Router();
-import ExampleModel from "../models/exampleModel.js";
+import UserModel from "../models/userModel.js";
 
-router.get("/examples", async (req, res) => {
+router.get("/users:id", async (req, res) => {
   try {
-    const examples = await ExampleModel.find();
+    const id = req.params.id;
+
+    const user = await UserModel.findOne({ _id: id });
+
+    if (!user) {
+      // If no example is found, return a 404 status
+      res.status(404).json({ error: "Example not found" });
+      return;
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.post("/users", async (req, res) => {
+  try {
+    // Create a new example using the provided data
+    const newExample = new UserModel({
+      name: "John",
+      description: "He likes apples",
+    });
+
+    // Save the new example to the database
+    const savedExample = await newExample.save();
+
+    // Retrieve all examples after adding the new one
+    const examples = await UserModel.find();
+
+    // Respond with the updated list of examples
     res.json(examples);
   } catch (error) {
     console.error(error);
--- server/server.js
+++ server/server.js
@@ -6,38 +6,14 @@ import apiRoutes from "./routes/apiRoutes.js";
 import dotenv from "dotenv";
 import path from "path";
 
-dotenv.config({ path: path.join("../.env") });
+dotenv.config({ path: path.join("./.env") });
 
 const app = express();
 const PORT = 3001;
 const DATABASEP = process.env.DATABASEP;
 
 // Replace <YOUR_CONNECTION_STRING> with your actual MongoDB Atlas connection string
-const uri = `mongodb+srv://jovanstosic012:${DATABASEP}@cluster0.ymcvseh.mongodb.net/?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const uri = `mongodb+srv://jovanstosic012:${DATABASEP}@cluster0.ymcvseh.mongodb.net/webflix?retryWrites=true&w=majority`;
 
 mongoose.connect(uri);
 
@@ -47,3 +23,22 @@ app.use("/api", apiRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const postData = async () => {
+  try {
+    const response = await fetch("http://localhost:3001/api/examples", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error posting data:", error);
+  }
+};
+
+// Call the postData function to make a POST request
+postData();
