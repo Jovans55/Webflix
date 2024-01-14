10ed50c82919dd0aafaa9a86919fda803dc5188f
Jovan <jovanstosic012@gmail.com>
2024-01-14 15:57:36 -0600

    Added some items to the menu and a toggle btn to see it or not see it

--- src/videoMain/VideoMain.jsx
+++ src/videoMain/VideoMain.jsx
@@ -4,9 +4,11 @@ import Cookies from "js-cookie";
 import VideoMenu from "./Videomenu";
 
 function VideoMain({ userData }) {
+  const [menu, setMenu] = useState(false);
   return (
     <div>
-      <VideoMenu />
+      {menu && <VideoMenu />}
+      <button onClick={() => setMenu(!menu)}>btn</button>
       <h1>{userData.firstName}</h1>
     </div>
   );
--- src/videoMain/VideoMenu.jsx
+++ src/videoMain/VideoMenu.jsx
@@ -1,33 +1,10 @@
 function VideoMenu() {
   return (
     <div>
-      <h1
-        style={{
-          backgroundColor: "white",
-          padding: "4px",
-          borderRadius: "50px",
-          width: "40px",
-          marginLeft: "10px",
-        }}
-      ></h1>
-      <h1
-        style={{
-          backgroundColor: "white",
-          padding: "4px",
-          borderRadius: "50px",
-          width: "40px",
-          marginLeft: "10px",
-        }}
-      ></h1>
-      <h1
-        style={{
-          backgroundColor: "white",
-          padding: "4px",
-          borderRadius: "50px",
-          width: "40px",
-          marginLeft: "10px",
-        }}
-      ></h1>
+      <h1>Account</h1>
+      <h1>Favorites</h1>
+      <h1>Settings</h1>
+      <h1>Cookies</h1>
     </div>
   );
 }
