6e2a78333cf56218e65fe6c175c6c3f2d5574cb3
Jovan <jovanstosic012@gmail.com>
2024-01-16 21:31:40 -0600

    Many many changes trying to make the slide in menu work next to the slideshow and videos

--- src/videoMain/VideoMain.jsx
+++ src/videoMain/VideoMain.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import VideoMenu from "./Videomenu";
+import Slideshow from "./SlideShow";
 
 function VideoMain({ userData }) {
   const [menu, setMenu] = useState(false);
   return (
     <div id="videoMain">
-      <section style={{ borderBottom: "5px solid #101012" }}>
+      <section id="videoHeader">
         <button id="btnBarsBtn" onClick={() => setMenu(!menu)}>
           <p className="btnBars"></p>
           <p className="btnBars"></p>
@@ -19,20 +20,29 @@ function VideoMain({ userData }) {
           style={{ margin: "5px 0px 0px 0px" }}
         />
       </section>
+      {menu && <VideoMenu />}
       <section id="videoMainBody">
-        {menu && <VideoMenu />}
-        <iframe
-          width="250"
-          height="200"
-          frameBorder="0"
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        ></iframe>
-        <iframe
-          width="250"
-          height="200"
-          frameBorder="0"
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        ></iframe>
+        <div id="videoMainBodyDiv">
+          <Slideshow />
+          <iframe
+            width="250"
+            height="200"
+            frameBorder="0"
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          ></iframe>
+          <iframe
+            width="250"
+            height="200"
+            frameBorder="0"
+            src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+          ></iframe>
+          <img
+            width="300"
+            height="100%"
+            src="https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg"
+            alt="YouTube Thumbnail"
+          />
+        </div>
       </section>
     </div>
   );
